064f516b758fffcb3cf204c9204889785b35eaaf
ecx <121473165+ecx2f@users.noreply.github.com>
2025-12-29 16:57:44 -0600

    style: integrate JetBrains Mono Nerd Font

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/ryanoasis/nerd-fonts@master/css/jetbrains-mono.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
